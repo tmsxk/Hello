06185e70e0dd9bfad730725b046fba726d100992
tmsxk <1985045185@qq.com>
2019-12-26 16:19:59 +0800

    a1

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello{
-    static String name="scxnb";
+    static String name="a1nb";
     public static void main(String[] args){
         System.out.println("hello from "+ name);
     }
